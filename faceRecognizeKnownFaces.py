4e0a19dcdb4082451fe923983bd6d0213332a553
Aytekin A Akkaya <59271994+ayteakkaya536@users.noreply.github.com>
2023-10-06 00:06:17 -0700

    typo correction

--- faceRecognizeKnownFaces.py
+++ faceRecognizeKnownFaces.py
@@ -20,7 +20,7 @@ with open('train.pkl','rb') as f: #rb stands for raw bit
     Encodings=pickle.load(f)
 
 font=cv2.FONT_HERSHEY_SIMPLEX
-cam=cv2.VideoCapture(1)
+cam=cv2.VideoCapture(0)
 
 while True:
     _,frame=cam.read()
@@ -28,7 +28,7 @@ while True:
     frameRGB=cv2.cvtColor(frameSmall,cv2,COLOR_BGR2RGB)
     facePositions=face_recognition.face_locations(frameRGB,model='cnn')
     allEncodings=face_recognition.face_encodings(frameRGB,facePositions)
-    for (top,rigt,bottom,left), face_encoding in zip(facePositions, allEncodings):
+    for (top,right,bottom,left), face_encoding in zip(facePositions, allEncodings):
         name='Unknown Person'
         matches=face_recognition.compare_faces(Encodings,face_encoding)
         if True in matches:
@@ -38,11 +38,11 @@ while True:
         right=right*3
         bottom-bottom*3
         left=left*3
-        cv2.rectaangle(frame,(left,top),(right,bottom),(0,0,255),2)
+        cv2.rectangle(frame,(left,top),(right,bottom),(0,0,255),2)
         cv2.putText(frame,name,(left,top-6),font,.75,(0,0,255),2)
     cv2.imshow('Picture',frame)
     cv2.moveWindow('Picture',0,0)
-    if cv2.waitKey(1)==ord('q)'):
+    if cv2.waitKey(1)==ord('q'):
         break
 cam.release()
 cv2.destroyAllWindows()
